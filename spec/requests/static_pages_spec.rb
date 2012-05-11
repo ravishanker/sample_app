ef29d1f3395b4f0e82e9f526e7b77ac3aae022ee
Ravi Shanker <ravi.m.shanker@gmail.com>
2012-05-11 21:02:40 +1000

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
+      #response.status.should be(200)
+    end
+  end
+  
+  describe "Home  page" do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "SampleApp | Home")
+    end
+    
+  end
+  
+  describe "Help page" do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help' 
+      page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "should have the title 'Help' " do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "SampleApp | Help")
+    end
+    
+  end
+  
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the title 'About Us' " do
+      visit '/static_pages/about' 
+      page.should have_selector('title', :text => "SampleApp | About Us")
+    end
+    
+  end
+  
+  
+  
+end
